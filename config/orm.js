75ac0c01105d399d3373d6749ccae4ed9fc649a5
Alexander Militar <militara106@gmail.com>
2019-09-25 20:18:46 -0700

    orm might be complete idk

--- config/orm.js
+++ config/orm.js
@@ -4,14 +4,25 @@ var orm = {
         var qeury = "SELECT * FROM ??";
         connection.query(query, [table], function (err, res) {
             if (err) throw err;
-            console.log(res);
+            return res;
         });
     },
     insertOne: function (table,burger_name) {
-        var query = "INSERT INTO ?? (burger_name) VALUES (?)"
-        // connection.query(query, [table]);
+        var query = "INSERT INTO ?? (burger_name) VALUES (?)";
+        connection.query(query, [table,burger_name],function(err,res){
+            if(err) throw err;
+            console.log(burger_name+" inserted into " + table);
+            return res;
+        });
     },
-    updateOne: function (burger_name, target, value) {}
+    updateOne: function (table,burger_name, target, value) {
+        var query = "UPDATE ?? SET ?? =? WHERE burger_name =?";
+        connection.query(query,[table,target,value,burger_name],function(err,res){
+            if(err) throw err;
+            console.log("Updated "+target+" with value "+value+" for "+burger_name);
+            return res;
+        })
+    }
 }
 
 module.exports = orm;
