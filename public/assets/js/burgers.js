5f3d8469b61d8f813e7aca808fe9393609fb5be5
Alexander Militar <militara106@gmail.com>
2019-09-27 17:36:35 -0700

    public folder bug fix

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -11,5 +11,5 @@ $(document).ready(function(){
             location.reload();
             console.log(id+" eaten");
         });
-    })
+    });
 });
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var app = express();
 
 var PORT = process.env.PORT || 8080;
 
+app.use(express.static("public"));
+
 app.use(express.urlencoded({
     extended: true
 }));
