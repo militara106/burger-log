34d3fa85e437791f0403baf107731a5c8ec2e2ac
Alexander Militar <militara106@gmail.com>
2019-09-28 11:36:04 -0700

    heroku fix attemplt #53

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ app.set("view engine", "handlebars");
 
 app.use(routes);
 
-app.listen(process.env.PORT, function () {
+app.listen(process.env.PORT || 8080, function () {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
