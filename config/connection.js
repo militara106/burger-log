e1274aadc20851152eca2980a4a58da510171780
Alexander Militar <militara106@gmail.com>
2019-09-28 10:26:12 -0700

    heroku fix attemplt #52

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 var mysql = require("mysql");
 var connection = mysql.createConnection({
-    host: "localhost",
+    host: "127.0.0.1",
     port: 3306,
     user: "root",
     password: "amilitar106",
